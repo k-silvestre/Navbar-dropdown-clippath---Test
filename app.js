848e3838d4cb68771c2a48c239e5b48b2705aa91
k-silvestre <krischristophersilvestre@gmail.com>
2023-05-23 16:56:45 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// DOM Elements
+
+const darkButton = document.getElementById('dark');
+const lightButton = document.getElementById('light');
+const solarButton = document.getElementById('solar');
+const body = document.body;
+
+//Button Event Handlers
+
+darkButton.onclick = () => {
+    body.classList.replace('light', 'dark');
+
+}
+
+lightButton.onclick = () => {
+    body.classList.replace('dark', 'light');
+
+}
+
+solarButton.onclick = () => {
+    
+}
